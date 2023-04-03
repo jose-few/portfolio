1b86c6c1b45aaa3a07c1565c5b8064dacb3b3341
joseph-few <joseph.few@hawksmoorim.co.uk>
2023-04-03 15:55:13 +0100

    Updated fading to be on a per element basis.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,31 +2,35 @@
     import { inview } from 'svelte-inview';
     import type { ObserverEventDetails, Options } from 'svelte-inview';
 
-    let isInView: boolean;
     const options: Options = {
         rootMargin: '-200px',
         unobserveOnEnter: false,
     };
 
     const show = ({detail}: CustomEvent<ObserverEventDetails>) => {
+        let isInView: boolean;
         (isInView = detail.inView);
-        console.log(isInView);
-        console.log(detail.node);
+        //console.log(detail.node);
+        if (isInView) {
+            detail.node.classList.add('show');
+        } else {
+            detail.node.classList.remove('show');
+        }
     };
 </script>
 
-<div use:inview={options} on:inview_change={show}>
-    <h1 class="hide" class:show="{isInView}">
+<div class="hide" use:inview={options} on:inview_change={show}>
+    <h1>
         Welcome.
     </h1>
 </div>
-<div use:inview={options} on:inview_change={show}>
-    <p class="hide" class:show="{isInView}">
+<div class="hide" use:inview={options} on:inview_change={show}>
+    <p>
         This is my website.
     </p>
 </div>
-<div use:inview={options} on:inview_change={show}>
-    <p class="hide" class:show="{isInView}">
+<div class="hide" use:inview={options} on:inview_change={show}>
+    <p>
         This site is still under construction!
     </p>
 </div>
@@ -34,18 +38,18 @@
 <div class="svgCont svg1"></div>
 
 <div class="spacer">
-    <div use:inview={options} on:inview_change={show}>
-        <p class="hide" class:show="{isInView}">
+    <div class="hide" use:inview={options} on:inview_change={show}>
+        <p>
             This is some text.
         </p>
     </div>
-    <div use:inview={options} on:inview_change={show}>
-        <p class="hide" class:show="{isInView}">
+    <div class="hide" use:inview={options} on:inview_change={show}>
+        <p>
             This is some more text.
         </p>
     </div>
-    <div use:inview={options} on:inview_change={show}>
-        <p class="hide" class:show="{isInView}">
+    <div class="hide" use:inview={options} on:inview_change={show}>
+        <p>
             I will probably put my projects here.
         </p>
     </div>
