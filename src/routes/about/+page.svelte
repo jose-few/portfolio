14f626e2b6e76cf6255a43618144fb1ca65f6339
Joseph <joseph.few27@gmail.com>
2023-04-09 02:25:16 +0100

    SOme more CSS changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,20 +8,22 @@
     </h1>
 </div>
 <img src="/IMG_5905.JPG" alt="Me!">
-<Typewriter keepCursorOnFinish={true} delay={500} mode={"cascade"} interval={80} showCursorOnDelay={true}>
-    <p>
-        I am a developer from Devon, England. I studied IT at Aberystwyth University, where 
-        I developed a passion for programming. Since graduating, I have been working as a 
-        Junior Developer at Hawksmoor, where I have been developing not just code - but my 
-        skills as well.
-    </p>
-    <p style="margin-top: 3%;">
-        My skillset includes JS, PHP, and more! I have hand crafted this portfolio using 
-        the JS framework Svelte. Please contact me if you have any queries or just want 
-        to chat!
-    </p>
-</Typewriter>
+<div class="tw">
+    <Typewriter keepCursorOnFinish={false} delay={500} mode={"cascade"} interval={60} showCursorOnDelay={true}>
+        <p>
+            I'm a developer from Devon, England. I studied IT at Aberystwyth University, where I developed a passion for programming. Since graduating, I have been working as a Junior Developer at Hawksmoor, where I've been developing not just code - but my skills as well. I'm driven and hard working, and always eager to improve!
+        </p>
+    </Typewriter>
+    <Typewriter keepCursorOnFinish={true} delay={20500} mode={"cascade"} interval={60} showCursorOnDelay={false}>
+        <p style="margin-top: 3%;">
+            My skillset includes JS, PHP, and more! I have hand crafted this portfolio using the JS framework Svelte. Please contact me if you have any queries or just want to chat!
+        </p>
+    </Typewriter>
+</div>
 <style>
+    .tw {
+        height: 300px;
+    }
     img {
         display: block;
         object-fit: cover;
@@ -59,14 +61,50 @@
         transform: scaleX(1);
         transform-origin: bottom left;
     }
-    @media screen and (max-width: 500px) {
-        img {
-            width: 75%;
+    @media screen and (max-width: 1350px) {
+        .tw {
+            height: 400px;
+        }
+    }
+    @media screen and (max-width: 1200px) {
+        .tw {
+            height: 600px;
+        }
+    }
+    @media screen and (max-width: 800px) {
+        .tw {
+            height: 600px;
+        }
+    }
+    @media screen and (max-width: 700px) {
+        .tw {
+            height: 600px;
         }
     }
     @media screen and (max-width: 600px) {
         img {
             width: 65%;
+        }
+        .tw {
+            height: 500px;
+        }
+    }
+    @media screen and (max-width: 500px) {
+        img {
+            width: 75%;
+        }
+        .tw {
+            height: 400px;
+        }
+    }
+    @media screen and (max-width: 400px) {
+        .tw {
+            height: 400px;
+        }
+    }
+    @media screen and (max-width: 360px) {
+        .tw {
+            height: 500px;
         }
     }
 </style>
